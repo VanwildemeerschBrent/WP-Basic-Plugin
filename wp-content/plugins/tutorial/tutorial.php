75f20c2d3c2cf264347422142372d21eaeba38cd
Brent Vanwildemeersch <brent.vanwildemeersch@easyfairs.com>
2022-09-11 19:47:03 +0200

    feature(CustomPlugin): Setup plugin files

---
+++ wp-content/plugins/tutorial/tutorial.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name: Tutorial
+ * Description: The Elementor Website Builder has it all: drag and drop page builder, pixel perfect design, mobile responsive editing, and more. Get started now!
+ * Plugin URI: https://vanwildemeerschbrent.github.io
+ * Author: Brent Vanwildemeersch
+ * Version: 0.1
+ * Author URI: https://vanwildemeerschbrent.github.io
+ * License : GPL2
+ * Text Domain: tutorial
+ */
+
+// security to prevent direct access to php files
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Create_tutorials_cpt
+ *
+ * @return void
+ */
+function create_Tutorials_cpt()
+{
+
+}
